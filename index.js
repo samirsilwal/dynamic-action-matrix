d6125a37e1f9ee3ea22fbc5381153e6fa024c9e8
samirsilwal <samirsilwal2424@gmail.com>
2024-10-15 08:18:00 +0545

    Add example with matrxi strategy

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const core = require('@actions/core');
 
 try {
   // Get input value
-  const exampleInput = core.getInput('value');
-  console.log(`Received input: ${exampleInput}`);
+  const inpputString = core.getInput('value');
+  const splittedString = inpputString.split(',');
 
   // Set an output value
-  const exampleOutput = `Output derived from ${exampleInput}`;
-  core.setOutput('parsed', exampleOutput);
+  const exampleOutput = `Output derived from ${splittedString}`;
+  console.log("🚀 ~ exampleOutput:", exampleOutput)
+  core.setOutput('parsed', splittedString);
 
 } catch (error) {
   core.setFailed(error.message);
